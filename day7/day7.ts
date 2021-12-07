3a549e9a04cc3d25eede9a9ef3db358556433dcc
rodrigosa7 <digex2008@live.com.pt>
2021-12-07 11:10:31 +0000

    day7 solution

---
+++ day7/day7.ts
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+var puzzle: Array<number> = [];
+fs.readFile("input.txt", "utf8", (err: string, data: string) => {
+  if (err) {
+    console.log(err);
+  }
+  puzzle = data.split(",").map(Number);
+
+  const mid = Math.floor(puzzle.length / 2),
+    nums = [...puzzle].sort((a, b) => a - b);
+  var median =
+    puzzle.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  var gas = 0;
+  puzzle.forEach((element) => {
+    if (element != median) {
+      gas += Math.abs(element - median);
+    }
+  });
+  var part2 = 0;
+  var acc = 1;
+
+  var coisa = Math.floor(
+    puzzle.reduce((acc, curr) => acc + curr, 0) / puzzle.length
+  );
+  console.log("COISA", coisa);
+
+  part2 = puzzle.reduce((acc, val) => {
+    const distance = Math.abs(val - coisa);
+    const consumption = (distance * (distance + 1)) / 2;
+
+    return acc + consumption;
+  }, 0);
+  console.info("PART2", part2);
+});
